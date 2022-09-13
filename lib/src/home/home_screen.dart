810827add6fd956a1f53b268276e74582d1d9545
Alexandre Fernandes <alexfernandes199922@gmail.com>
2022-09-13 11:00:23 -0300

    feat: added search characters implementation

--- lib/src/home/home_screen.dart
+++ lib/src/home/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mottu/shared/helpers/snack_bar_helper.dart';
-import 'package:mottu/shared/themes/default/default_pallete.dart';
+import 'package:mottu/shared/themes/default/default_theme.dart';
 import 'package:mottu/src/home/home_controller.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -27,64 +27,114 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("I'm Mottu"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(height: 16),
-          Obx(
-            () => controller.isLoading.value
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : Expanded(
-                    child: GridView.builder(
-                        itemCount: controller.charactersList.length,
-                        scrollDirection: Axis.horizontal,
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 5,
-                        ),
-                        itemBuilder: (_, i) {
-                          var character = controller.charactersList[i];
-                          return InkWell(
-                            splashColor: PalleteColor.white,
-                            onTap: () {
-                              Get.toNamed('/details', arguments: {
-                                'name': character.name,
-                                'imageThumbnail': character.thumbnail,
-                                'description': character.description,
-                              });
-                            },
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Expanded(
-                                  child: Image.network(
-                                    '${character.thumbnail!.path}.${character.thumbnail!.extension}',
-                                    width: 50,
-                                    height: 50,
-                                    fit: BoxFit.fill,
+        appBar: AppBar(
+          title: const Text("I'm Mottu"),
+        ),
+        body: Obx(
+          () => controller.isLoading.value
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 16),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: SizedBox(
+                              height: 45,
+                              child: TextFormField(
+                                style: DefaultFontStyle.paragraphSmall.getText
+                                    .copyWith(
+                                  color: PalleteColor.grayPrimary,
+                                ),
+                                controller: controller.searchController.value,
+                                scrollPadding:
+                                    const EdgeInsets.symmetric(vertical: 30),
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(6),
+                                    borderSide: const BorderSide(
+                                      width: 1,
+                                      color: PalleteColor.grayPrimary,
+                                    ),
                                   ),
                                 ),
-                                Flexible(
-                                  child: Text(
-                                    character.name!,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                              ],
+                              ),
                             ),
-                          );
-                        }),
-                  ),
-          ),
-        ],
-      ),
-    );
+                          ),
+                          const SizedBox(width: 16),
+                          SizedBox(
+                            height: 45,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                if (controller
+                                    .searchController.value.text.isEmpty) {
+                                  SnackbarHelper.showFailure(
+                                    context,
+                                    message: 'Search field is required',
+                                  );
+                                } else {
+                                  await controller.getSearchCharacters(
+                                    search:
+                                        controller.searchController.value.text,
+                                  );
+                                }
+                              },
+                              child: const Text('Search'),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: GridView.builder(
+                          itemCount: controller.charactersList.length,
+                          scrollDirection: Axis.horizontal,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                          ),
+                          itemBuilder: (_, i) {
+                            var character = controller.charactersList[i];
+                            return InkWell(
+                              splashColor: PalleteColor.white,
+                              onTap: () {
+                                Get.toNamed('/details', arguments: {
+                                  'name': character.name,
+                                  'imageThumbnail': character.thumbnail,
+                                  'description': character.description,
+                                });
+                              },
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Expanded(
+                                    child: Image.network(
+                                      '${character.thumbnail!.path}.${character.thumbnail!.extension}',
+                                      width: 50,
+                                      height: 50,
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                  Flexible(
+                                    child: Text(
+                                      character.name!,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }),
+                    ),
+                  ],
+                ),
+        ));
   }
 }
 
