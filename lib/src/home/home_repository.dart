bab91bc13c68e898693d385e918ef5fbafa678fb
Alexandre Fernandes <alexfernandes199922@gmail.com>
2022-09-14 09:44:38 -0300

    feat: added dio cache

--- lib/src/home/home_repository.dart
+++ lib/src/home/home_repository.dart
@@ -1,17 +1,21 @@
 import 'package:dio/dio.dart';
 import 'package:mottu/shared/helpers/url_helper.dart';
 import 'package:mottu/shared/service/custom_dio.dart';
+import 'package:mottu/shared/service/dio_cache.dart';
 
 class HomeRepository {
   Dio dio = CustomDio().dio;
+  DioCache dioCache = DioCache();
   UrlHelper urlHelper = UrlHelper();
 
   Future getCharacters() async {
-    //Note -> Ultimos personagens que foram modificidos na api da Marvel
-
+    //NOTE -> Ultimos personagens que foram modificados na api da Marvel
     String finalUrl =
         urlHelper.generateUrl(path: 'characters', params: '&orderBy=modified');
-    return await dio.get('/$finalUrl');
+
+    dio.interceptors.add(dioCache.dioCacheManager.interceptor);
+
+    return await dio.get('/$finalUrl', options: dioCache.cacheOptions);
   }
 
   Future getSearchCharacters({required String search}) async {
